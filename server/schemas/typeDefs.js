8ea1303db0aeaaba1fa58400900e0e9a8ff6505f
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-11-14 23:11:00 -0500

    worked on typedefs and defined the necessary Query and Mutation types

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    bookCount: String
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input SavedBookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SavedBookInput): User
+    removeThought(bookId: String!): User 
+  }
+`;
+
+module.exports = typeDefs;
