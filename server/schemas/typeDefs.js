4f433b642171b578edecbc9a7abd5053fe494857
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-11-14 23:12:32 -0500

    corrected removethought to removeBook in typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     saveBook(input: SavedBookInput): User
-    removeThought(bookId: String!): User 
+    removeBook(bookId: String!): User 
   }
 `;
 
